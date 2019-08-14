ae93ff74b3c4c256bc34acc4044ac56d5e78de0d
ian-sanchez <silkyhairbear@gmail.com>
2019-08-14 13:57:28 -0700

    finsihed componenets

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+import Card from "./components.js";
 import "./App.css";
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <Card Title="prop title" url="prop url" description="blah blah blah"/>
     </div>
   );
 }
---
+++ src/components.js
@@ -0,0 +1,12 @@
+import React from "react";
+function Card(props){
+    console.log(props);
+    return (
+        <div>
+            <h1>{props.title}</h1>
+            <img src={props.url} />
+            <p>{props.explanation}</p>
+        </div>
+    );
+}
+export default Card;
