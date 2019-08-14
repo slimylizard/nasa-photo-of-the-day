58a0059a00fa935336bbb2f77143f194528d6890
ian-sanchez <silkyhairbear@gmail.com>
2019-08-14 15:34:48 -0700

    ian-sanchez

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import Card from "./components.js";
 import "./App.css";
 
 function App() {
+  const [data, setData] = useState({});
+  useEffect(()=>{
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(res =>{
+      console.log(res.data);
+      setData(res.data);
+    });
+  }, []);
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-      <Card Title="prop title" url="prop url" description="blah blah blah"/>
+      <Card title={data.title} url={data.url} explanation={data.explanation}/>
     </div>
   );
 }
--- src/components.js
+++ src/components.js
@@ -4,7 +4,7 @@ function Card(props){
     return (
         <div>
             <h1>{props.title}</h1>
-            <img src={props.url} />
+            <img src={props.url} alt=" " />
             <p>{props.explanation}</p>
         </div>
     );
